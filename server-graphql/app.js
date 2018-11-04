2ab0a9d3a2b73a8dfa6ff6a89baaaa17740b5b5c
Ayush Goyal <ayush987goyal@gmail.com>
2018-11-04 11:31:16 +0530

    Added basic setup for gql

---
+++ server-graphql/app.js
@@ -0,0 +1,49 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const multer = require('multer');
+
+const app = express();
+
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().toISOString() + '-' + file.originalname);
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  const isImage =
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg';
+  cb(null, isImage);
+};
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(multer({ storage: fileStorage, fileFilter: fileFilter }).single('image'));
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({ message: message, data: data });
+});
+
+mongoose
+  .connect(
+    process.env.MONGODB_URI,
+    { useNewUrlParser: true }
+  )
+  .then(result => {
+    app.listen(8080, () => console.log('Server running on http://localhost:8080'));
+  })
+  .catch(err => console.log(err));
