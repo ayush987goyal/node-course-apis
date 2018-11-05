211438bba1ca0d55e5c98e068411f0e331f2b153
Ayush Goyal <ayush987goyal@gmail.com>
2018-11-05 10:35:13 +0530

    Add delete post func

--- server-graphql/app.js
+++ server-graphql/app.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const fs = require('fs');
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -11,6 +10,7 @@ const graphqlHttp = require('express-graphql');
 const graphqlSchema = require('./graphql/schema');
 const graphqlResolvers = require('./graphql/resolvers');
 const auth = require('./middleware/auth');
+const { clearImage } = require('./util/file');
 
 const app = express();
 
@@ -88,8 +88,3 @@ mongoose
     app.listen(8080, () => console.log('Server running on http://localhost:8080'));
   })
   .catch(err => console.log(err));
-
-const clearImage = filePath => {
-  filePath = path.join(__dirname, '..', filePath);
-  fs.unlink(filePath, err => console.log(err));
-};
