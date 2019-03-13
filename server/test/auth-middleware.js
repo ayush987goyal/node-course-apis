d005df221e7ce63e10c14ff96abd5ceb0c2c4554
Ayush Goyal <ayush987goyal@gmail.com>
2019-03-14 00:10:11 +0530

    Add another test and describe

--- server/test/auth-middleware.js
+++ server/test/auth-middleware.js
@@ -2,12 +2,24 @@ const { expect } = require('chai');
 
 const authMiddleware = require('../middleware/is-auth');
 
-it('should throw an error if no Authorization header is present', () => {
-  const req = {
-    get(headerName) {
-      return null;
-    }
-  };
+describe('Auth middleware', () => {
+  it('should throw an error if no Authorization header is present', () => {
+    const req = {
+      get(headerName) {
+        return null;
+      }
+    };
 
-  expect(authMiddleware.bind(this, req, {}, () => {})).to.throw('Not Authenticated.');
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw('Not Authenticated.');
+  });
+
+  it('should throw an error if Authorization header is only one string', () => {
+    const req = {
+      get(headerName) {
+        return 'xyz';
+      }
+    };
+
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+  });
 });
