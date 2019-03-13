c4f8d44f4666b648ad7695b312cd27e372aa29ea
Ayush Goyal <ayush987goyal@gmail.com>
2019-03-14 00:23:56 +0530

    Add check for token and sinon for stub

--- server/test/auth-middleware.js
+++ server/test/auth-middleware.js
@@ -1,4 +1,6 @@
 const { expect } = require('chai');
+const sinon = require('sinon');
+const jwt = require('jsonwebtoken');
 
 const authMiddleware = require('../middleware/is-auth');
 
@@ -17,6 +19,35 @@ describe('Auth middleware', () => {
     const req = {
       get(headerName) {
         return 'xyz';
+      }
+    };
+
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+  });
+
+  it('should yeild a userId after decoding the token', () => {
+    const req = {
+      get(headerName) {
+        return 'Bearer sfsgercbrtgdfg';
+      }
+    };
+
+    sinon.stub(jwt, 'verify');
+    jwt.verify.returns({ userId: 'abc' });
+
+    authMiddleware(req, {}, () => {});
+
+    expect(req).to.have.property('userId');
+    expect(req).to.have.property('userId', 'abc');
+    expect(jwt.verify.called).to.be.true;
+
+    jwt.verify.restore();
+  });
+
+  it('should throw an error if the token cannot be verified', () => {
+    const req = {
+      get(headerName) {
+        return 'Bearer xyz';
       }
     };
 
