f9a641afc289ba2ba92bc4e5cc29dcde12fedb48
Ayush Goyal <ayush987goyal@gmail.com>
2019-03-14 08:45:28 +0530

    Add test for user status wth actual test db

--- server/test/auth-controller.js
+++ server/test/auth-controller.js
@@ -1,5 +1,6 @@
 const { expect } = require('chai');
 const sinon = require('sinon');
+const mongoose = require('mongoose');
 
 const User = require('../models/user');
 const AuthController = require('../controllers/auth');
@@ -23,5 +24,42 @@ describe('Auth Controller - Login', () => {
     });
 
     User.findOne.restore();
+  });
+
+  it('should send a response with valid user status for an existing user', done => {
+    mongoose
+      .connect(process.env.MONGODB_TEST_URI, { useNewUrlParser: true })
+      .then(() => {
+        const user = new User({
+          email: 'tet@test.com',
+          password: 'tester',
+          name: 'Test',
+          posts: [],
+          _id: '5c0f66b979af55031b34728a'
+        });
+
+        return user.save();
+      })
+      .then(() => {
+        const req = { userId: '5c0f66b979af55031b34728a' };
+        const res = {
+          statusCode: 500,
+          userStatus: null,
+          status(code) {
+            this.statusCode = code;
+            return this;
+          },
+          json(data) {
+            this.userStatus = data.status;
+          }
+        };
+
+        AuthController.getStatus(req, res, () => {}).then(() => {
+          expect(res.statusCode).to.be.equal(200);
+          expect(res.userStatus).to.be.equal('I am new!');
+          done();
+        });
+      })
+      .catch(err => console.log(err));
   });
 });
