79e8e4a302fa10ec73edf60ef6dfa45f7d44a4b2
Ayush Goyal <ayush987goyal@gmail.com>
2019-03-14 08:33:41 +0530

    Add test for auh cont with async

---
+++ server/test/auth-controller.js
@@ -0,0 +1,27 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+
+const User = require('../models/user');
+const AuthController = require('../controllers/auth');
+
+describe('Auth Controller - Login', () => {
+  it('should throw an error with code 500 if acessing database fails', done => {
+    sinon.stub(User, 'findOne');
+    User.findOne.throws();
+
+    const req = {
+      body: {
+        email: 'test@test.com',
+        password: 'test123'
+      }
+    };
+
+    AuthController.login(req, {}, () => {}).then(result => {
+      expect(result).to.be.an('error');
+      expect(result).to.have.property('statusCode', 500);
+      done();
+    });
+
+    User.findOne.restore();
+  });
+});
